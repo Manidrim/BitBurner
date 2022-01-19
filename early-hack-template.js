4d41cbd03414f725f66563af76e05dd56de751ce
Manidrim <emmanuel_jm_meyer@hotmail.fr>
2022-01-19 20:10:55 +0100

    Update early-hack-template.js

--- early-hack-template.js
+++ early-hack-template.js
@@ -1,18 +1,71 @@
+let globalNs;
+let argsZero;
+
+
+export function myPrint(toPrint) {
+	globalNs.tprint(toPrint);
+}
+
 export async function main(ns) {
-    var target = ns.args[0];
-    var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
-    var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
-    if (ns.fileExists("BruteSSH.exe", "home")) {
-        ns.brutessh(target);
-    }
-    ns.nuke(target);
-    while(true) {
-        if (ns.getServerSecurityLevel(target) > securityThresh) {
-            await ns.weaken(target);
-        } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
-            await ns.grow(target);
-        } else {
-            await ns.hack(target);
-        }
-    }
+	globalNs = ns;
+    argsZero = ns.args[0];
+	
+	myPrint('The START on ' + argsZero);
+	
+	let hostName = globalNs.getHostname();
+	let scans = globalNs.scan();
+	let isFileExist = false;
+
+
+    this.toPrint
+	myPrint(hostName);
+	myPrint(scans);
+    
+
+	for(let i = 0; i < scans.length; i++) {
+		let serverToHack = scans[i];
+		myPrint(serverToHack);
+		isFileExist = globalNs.fileExists('early-hack-template.js', serverToHack);
+		if (isFileExist) {
+			myPrint('File exist');
+			continue;
+		}
+
+		myPrint('File doesn\'t exist');
+		let securityLevel = globalNs.getServerSecurityLevel(serverToHack);
+		myPrint('securityLevel : ' . securityLevel);
+		
+
+/*
+		if (ns.fileExists("BruteSSH.exe", "home")) {
+			ns.brutessh(serverToHack);
+		}
+		if (ns.fileExists("FTPCrack.exe", "home")) {
+			ns.brutessh(serverToHack);
+		}
+*/
+		
+/*
+		var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
+		var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+		if (ns.fileExists("BruteSSH.exe", "home")) {
+			ns.brutessh(target);
+		}
+		ns.nuke(target);
+		if (ns.getServerSecurityLevel(target) > securityThresh) {
+			myPrint(ns, 'weaken');
+			await ns.weaken(target);
+		} else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+			myPrint(ns, 'grow');
+			await ns.grow(target);
+		} else {
+			myPrint(ns, 'hack');
+			await ns.hack(target);
+			ns.run('Nuke.exe');
+		}
+*/
+	}
+	
+
+	myPrint('The END on ' + argsZero);
 }
